94b76f35601af64ad31bfa8c98abad7f5d952d33
yitendra <kumar.yitendra@gmail.com>
2017-09-13 22:21:34 +0530

    color version 2

--- color/app.js
+++ color/app.js
@@ -2,18 +2,19 @@ console.log('Script is Working');
 var body;
 body=document.getElementById("container");
 
-var r=225;
-var g=0;
-var b=0;
+var r=225,g=0,b=0;
 
-
-var color='010101';
 function bgcolor(){
 	body.style.background = "rgb("+r+","+g+","+b+")";
-	if(r===226){r=0;g=0;b=0;}
-	else{
-		if(g===225){b++; r--;}
+	if(r===225 && g<225){
+		if(b!=0)b--;
 		else g++;
+	}else if(g===225 && b<225){
+		if(r!=0)r--;
+		else b++;
+	}else if(b===225 && r<225){
+		if(g!=0)g--;
+		else r++;
 	}
 		
 }
