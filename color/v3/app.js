924d44dc0d072f48cc156ca359321e81923234af
YITENDRA KUMAR <kumar.yitendra@gmail.com>
2018-06-09 12:04:35 +0530

    Designed Color Versions

---
+++ color/v3/app.js
@@ -0,0 +1,27 @@
+console.log('Script is Working');
+var body;
+body=document.getElementById("container");
+
+var r=225,g=0,b=0;
+
+function bgcolor(){
+	body.style.background = "radial-gradient(rgb("+r+","+g+","+b+"),rgb("+g+","+b+","+r+"))" //"rgb("+r+","+g+","+b+")";
+	if(r===225 && g<225){
+		if(b!=0)b--;
+		else g++;
+	}else if(g===225 && b<225){
+		if(r!=0)r--;
+		else b++;
+	}else if(b===225 && r<225){
+		if(g!=0)g--;
+		else r++;
+	}
+		
+}
+
+var interval;
+function changeColor(){
+interval=setInterval(function(){bgcolor();}, 10);
+}
+
+changeColor();
